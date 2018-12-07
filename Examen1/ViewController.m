60d8018e2e31dcc7fff98d9b6ad0d0cd8df56786
CarlosAlonsoHCh <1413144@utp.edu.pe>
2018-12-07 16:59:53 -0500

    Connection  URL del listado de tickets (archivo JSON)

--- Examen1/ViewController.m
+++ Examen1/ViewController.m
@@ -10,14 +10,51 @@
 
 @interface ViewController ()
 
+@property(nonatomic,strong) NSString*name;//@"value"
+@property(nonatomic,strong) NSString*day;//@value
+@property(nonatomic,strong) NSString*address;
+@property(nonatomic,strong) NSString*startTime;
+
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self RequestListTicket];
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"hola");
+    
+}
+
+- (void)RequestListTicket{
+    NSURL*urlTicket = [NSURL URLWithString:@"https://api.myjson.com/bins/tp1am"];
+    NSMutableURLRequest*request = [NSMutableURLRequest requestWithURL:urlTicket
+                                                          cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60.0];
+    
+    [request setHTTPMethod:@"GET"];
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration delegate:self delegateQueue:nil];
+    
+    NSURLSessionDataTask *postdataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
+    
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+        
+        if (error==nil){
+            if([httpResponse statusCode] == 200){
+                
+                NSDictionary*dictFromData=[NSJSONSerialization JSONObjectWithData:data  options:NSJSONReadingAllowFragments error:&error];
+                NSLog(@"Z");
+                
+            }
+        }
+    
+    }];
+    [postdataTask resume];
+    
 }
 
 
